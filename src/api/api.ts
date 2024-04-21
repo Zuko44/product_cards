ac7d8b3a390ffd651c97e20fbf905946657f4ea9
Vladislav1992 <kuryanov.vlad@yandex.ru>
2024-04-21 19:48:31 +0300

    removal added

--- src/api/api.ts
+++ src/api/api.ts
@@ -7,3 +7,11 @@ export const getAddedProducts = async () => {
     console.log(error);
   }
 };
+
+export const deleteOneProduct = async (id: number) => {
+  const response = await fetch('https://fakestoreapi.com/products/' + id, {
+    method: 'DELETE',
+  });
+  const result = await response.json();
+  return result;
+};
--- src/components/AddedProducts.vue
+++ src/components/AddedProducts.vue
@@ -1,7 +1,7 @@
 <script setup lang="ts">
 import { ref, onMounted } from 'vue';
 import type { Product } from '../types/index';
-import { getAddedProducts } from '../api/api';
+import { getAddedProducts, deleteOneProduct } from '../api/api';
 
 const products = ref<Product[]>([]);
 const switchProducts = ref<boolean>(false);
@@ -11,6 +11,31 @@ const getProducts = async () => {
     products.value = result;
     console.log(result);
   });
+};
+
+const deleteProduct = (id: number) => {
+  const reallyDeleteOrNot = confirm(
+    'Вы действительно хотите удалить продукт с id ' + id + '?',
+  );
+  console.log(id);
+  console.log(products.value);
+  if (reallyDeleteOrNot) {
+    console.log(products.value);
+    deleteOneProduct(id).then((result) => {
+      if (result) {
+        console.log(result);
+        for (let product of products.value) {
+          if (product.id === id) {
+            products.value = products.value.filter(
+              (product) => product.id !== id,
+            );
+          }
+        }
+      } else {
+        alert('Произошла ошибка при удалении элемента с id ' + id);
+      }
+    });
+  }
 };
 
 onMounted(() => {
@@ -41,11 +66,14 @@ onMounted(() => {
         <th>Рейтинг</th>
         <th>Рейтинг(значение)</th>
         <th>Опубликовано</th>
+        <th>Удалить</th>
       </tr>
       <tr v-if="switchProducts" v-for="product in products">
         <template v-if="product.public">
           <td>
-            <RouterLink :to="'/edit/' + product.id">Редактировать</RouterLink>
+            <RouterLink :to="'/edit/' + product.id"
+              >Редактировать, id={{ product.id }}</RouterLink
+            >
           </td>
           <td>{{ product.id }}</td>
           <td>{{ product.category }}</td>
@@ -56,11 +84,20 @@ onMounted(() => {
           <td>{{ product.rating.rate }}</td>
           <td>{{ product.rating.count }}</td>
           <td>{{ product.public }}</td>
+          <td>
+            <a
+              :href="'product/' + product.id"
+              @click.prevent="deleteProduct(product.id)"
+              >Удалить, id={{ product.id }}</a
+            >
+          </td>
         </template>
       </tr>
       <tr v-if="!switchProducts" v-for="product in products">
         <td>
-          <RouterLink :to="'/edit/' + product.id">Редактировать</RouterLink>
+          <RouterLink :to="'/edit/' + product.id"
+            >Редактировать, id={{ product.id }}</RouterLink
+          >
         </td>
         <td>{{ product.id }}</td>
         <td>{{ product.category }}</td>
@@ -71,6 +108,13 @@ onMounted(() => {
         <td>{{ product.rating.rate }}</td>
         <td>{{ product.rating.count }}</td>
         <td>{{ product.public }}</td>
+        <td>
+          <a
+            :href="'product/' + product.id"
+            @click.prevent="deleteProduct(product.id)"
+            >Удалить, id={{ product.id }}</a
+          >
+        </td>
       </tr>
     </table>
   </div>
